36a23203dc0ccdb81320cfa098ebdf85e157f517
Daniel Tshipuk <Daniel Tshipuk@LAPTOP-AUCE291A>
2022-10-24 16:28:30 +0200

    Add project files.

---
+++ E-library/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_library.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
